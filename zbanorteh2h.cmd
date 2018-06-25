ae513f4ecc360ae037e278a8f99488fdc49bbb7e
Arturo Equihua Morales <AEQUIHUA@gtrimex.mx>
2018-06-25 18:38:09 -0500

    Correccion zbanorteh2h por ruta de ambiente productivo.

--- zbanorteh2h.cmd
+++ zbanorteh2h.cmd
@@ -1,7 +1,7 @@
 @echo off
-echo Mueve archivos de SAP GTMXQAS00 al H2H TEST MOLMEXH2H
+echo Mueve archivos de SAP CITXP al H2H TEST MOLMEXH2H
 echo.
-if exist \\GTMXQAS00\ArchivosBanorte\enviarH2H\PP*.TXT xcopy \\GTMXQAS00\ArchivosBanorte\enviarH2H\PP*.TXT \\molmexh2h\TESTdir\a_enviar /Z 
-if exist \\GTMXQAS00\ArchivosBanorte\enviarH2H\PP*.TXT move \\GTMXQAS00\ArchivosBanorte\enviarH2H\PP*.TXT \\GTMXQAS00\ArchivosBanorte\respaldoH2H
+if exist \\CITXP\ArchivosBanorte\enviarH2H\PP*.TXT xcopy \\CITXP\ArchivosBanorte\enviarH2H\PP*.TXT \\molmexh2h\dirH2H\a_enviar  /Z 
+if exist \\CITXP\ArchivosBanorte\enviarH2H\PP*.TXT move \\CITXP\ArchivosBanorte\enviarH2H\PP*.TXT \\CITXP\ArchivosBanorte\respaldoH2H
 
 
